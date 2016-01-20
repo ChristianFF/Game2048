c44f12d96ff06686db37b173b049eb19338ba7aa
费凡 <404619986@qq.com>
2016-01-20 16:24:56 +0800

    重构代码和界面

---
+++ README.md
@@ -0,0 +1,10 @@
+# 2048
+
+利用自定义的ViewGroup实现2048游戏棋盘
+
+#### 软件截图
+<img src="/screenshot/2048.png" alt="screenshot" title="screenshot" width="720" height="1280" />
+
+#### 参考
+- **鸿洋的博客**
+[带你玩转实现游戏2048 其实2048只是个普通的控件](http://blog.csdn.net/lmj623565791/article/details/40020137)
--- app/src/main/java/com/ff/ui/base/BaseActivity.java
+++ app/src/main/java/com/ff/ui/base/BaseActivity.java
@@ -1,12 +1,17 @@
 package com.ff.ui.base;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
-import com.ff.util.UIUtils;
+import com.ff.R;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 /**
  * 快速开发的activity基础框架类，提供了getView来简化findViewById,并且提供了构建框架.
@@ -35,7 +40,30 @@ public abstract class BaseActivity extends AppCompatActivity {
      * 在setContentView之前触发的方法
      */
     protected void beforeSetContentView() {
-        UIUtils.setUpSystemBar(this);
+        setUpSystembar();
+    }
+
+    private void setUpSystembar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            setTranslucentStatus(true);
+        }
+
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+        tintManager.setStatusBarTintEnabled(true);
+        tintManager.setStatusBarTintResource(R.color.colorPrimaryDark);
+    }
+
+    @TargetApi(19)
+    private void setTranslucentStatus(boolean on) {
+        Window win = getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
     }
 
     /**
@@ -53,7 +81,7 @@ public abstract class BaseActivity extends AppCompatActivity {
     /**
      * 在这里初始化设置view的各种资源，比如适配器或各种变量
      */
-    protected void beforeSetViews(){
+    protected void beforeSetViews() {
 
     }
 
