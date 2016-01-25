c9cb9224bbd7607d467dd54bfbf247340e24326a
费凡 <404619986@qq.com>
2016-01-25 14:15:33 +0800

    修改Dialog样式

--- app/src/main/java/com/ff/ui/MainActivity.java
+++ app/src/main/java/com/ff/ui/MainActivity.java
@@ -1,9 +1,9 @@
 package com.ff.ui;
 
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
